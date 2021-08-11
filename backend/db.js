e62e08d07f5c569dea1c2fca0ba36baca6343216
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-12 06:06:24 +0900

    배포 세팅

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const pool = mysql.createPool({
 	connectionLimit: 10,
-	host: "mysql",
-	user: "root",
-	password: "sunwoo",
-	database: "myapp",
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
 });
 exports.pool = pool;
