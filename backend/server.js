f5a178bf5bc121e8f43b54ab96e7358864b9b3ea
joraeng <60039286+joraeng@users.noreply.github.com>
2021-08-12 08:29:17 +0900

    왜 백엔드만 포트를 못찾는느낌이냐

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,24 @@
-// 필요한 모듈들을 가져온다
 const express = require("express");
 
 const db = require("./db");
 
-// Express 서버를 생성
 const app = express();
 
-// json 형태로 오는 요청의 본문을 해석해줄 수 있게 등록
-// app.use(bodyParser.json()), 이제부터는 body-parser도 express에 포함되어있음
 app.use(express.json());
 
-// // 테이블 생성하기
-// db.pool.query(
-// 	`CREATE TABLE lists (
-//     id INTEGER AUTO_INCREMENT,
-//     value TEXT,
-//     PRIMARY KEY (id)
-//     )`,
-// 	(err, results, fields) => {
-// 		console.log(`err:${err}`);
-// 		console.log(`results`, results);
-// 	}
-// );
+db.pool.query(
+	`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+    )`,
+	(err, results, fields) => {
+		console.log(`err:${err}`);
+		console.log(`results`, results);
+	}
+);
 
-//DB lists 테이블에 있는 모든 데이터를 프론트 서베에 보내주기
-app.get("/api/values", function (req, res) {
-	//데이테베이스에서 모든 정보 가져오기
+
 	console.log(`req:${req}`);
 	db.pool.query("SELECT * FROM lists;", (err, results, fileds) => {
 		console.log(`err:${err}`);
@@ -35,7 +28,6 @@ app.get("/api/values", function (req, res) {
 		else return res.json(results);
 	});
 });
-// 클라이언트에서 입력한 값을 데이터베이스 lists 테이블에 넣어주기
 app.post("/api/value", function (req, res) {
 	db.pool.query(
 		`INSERT INTO lists (value) VALUES("${req.body.value}");`,
@@ -48,8 +40,6 @@ app.post("/api/value", function (req, res) {
 		}
 	);
 });
-
-// Express 서버 포트 5050번 시작
 app.listen(5000, () => {
 	console.log("server listening 5000....");
 });
